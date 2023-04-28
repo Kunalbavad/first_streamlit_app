d5a4186d1f49bd2a365bc7c20d42a616638148f4
Kunalbavad <132040006+Kunalbavad@users.noreply.github.com>
2023-04-28 11:11:43 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,10 @@
 import streamlit
 
-streamlit.title ('Breakfast')
+streamlit.title ('My Parents New Diner')
 
-streamlit.title ('My Dinner')
+streamlit.header ('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
  
   
